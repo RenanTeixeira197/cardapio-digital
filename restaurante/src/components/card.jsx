75834ced911e62702da916a222729db9166c605c
Renan Teixeira <rteixeira1997@gmail.com>
2025-10-18 20:01:58 -0300

    update

--- restaurante/src/components/card.jsx
+++ restaurante/src/components/card.jsx
@@ -3,7 +3,10 @@ import Cards from "../components/cards";
 const card = () => {
   return (
     <div className="cards">
-        <Cards />
+        <Cards titulo="Burguer de Picanha"
+        descricao = "Burguer de Picanha Angus, Queijo Cheddar, Crisp de Cebola e Geleira de Bacon"
+        preco="R$ 44,90"
+        />
         <Cards />
         <Cards />
         <Cards />
